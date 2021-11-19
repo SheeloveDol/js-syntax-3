ca9e1a1e5fe4f4f3a6a36b13e1238c7eb154b20a
Sheelove Dol <siesel26@gmail.com>
2021-11-19 13:38:14 -0800

    Add surgeon class example

--- main.js
+++ main.js
@@ -29,3 +29,4 @@ console.log(halley.behavior); // Print behavior value to console
 halley.incrementBehavior(); // Add one to behavior
 console.log(halley.name); // Print name value to console
 console.log(halley.behavior); // Print behavior value to console
+
