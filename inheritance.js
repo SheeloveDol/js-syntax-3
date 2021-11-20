f3071eff254128c5105877df23140ca65d6db1cc
Sheelove Dol <siesel26@gmail.com>
2021-11-19 16:24:34 -0800

    Add example of static method

--- inheritance.js
+++ inheritance.js
@@ -15,13 +15,30 @@ class HospitalEmployee {
   takeVacationDays(daysOff) {
     this._remainingVacationDays -= daysOff;
   }
+
+  static generatePassword() {
+    return Math.floor(Math.random() * 10000);
+  }
 }
 
 class Nurse extends HospitalEmployee {
   constructor(name, certifications) {
     super(name);
     this._certifications = certifications;
+  } 
+  
+  get certifications() {
+    return this._certifications;
+  }
+  
+  addCertification(newCertification) {
+    this.certifications.push(newCertification);
   }
 }
 
-const nurseOlynyk = new Nurse('Olynyk', ['Trauma', 'Pediatrics']);
+const nurseOlynyk = new Nurse('Olynyk', ['Trauma','Pediatrics']);
+nurseOlynyk.takeVacationDays(5);
+console.log(nurseOlynyk.remainingVacationDays);
+nurseOlynyk.addCertification('Genetics');
+console.log(nurseOlynyk.certifications);
+console.log(HospitalEmployee.generatePassword())
