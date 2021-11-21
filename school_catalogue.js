a2613822a45749eef54678e0a3a979a1001372e3
Sheelove Dol <siesel26@gmail.com>
2021-11-20 22:09:24 -0800

    finished school catalogue project

--- school_catalogue.js
+++ school_catalogue.js
@@ -15,12 +15,30 @@ class School {
     return this._numberOfStudents
   }
 
-  set numberOfStudents(number) {
-    typeof number === 'Number' ? this._numberOfStudents = number : 'Must be number'
+  quickFacts() {
+    console.log(`${this._name} educates ${this._numberOfStudents} students at the ${this._level} school level.`);
   }
+
+  static pickSubstituteTeacher(substituteTeachers) {
+    // substituteTeachers = ['Julie', 'Arnoux', 'Carter', 'Walker'];
+    const randomSub = Math.floor(Math.random() * (substituteTeachers.length - 1))
+    return substituteTeachers[randomSub] 
+  }
+
+  set numberOfStudents(number) {
+    if (typeof number === 'number') { 
+    this._numberOfStudents = number
+      } else {
+   console.log('Invalid input: numberOfStudents must be set to a Number')
+    }
+  }
+
 }
 
-//Creating primary class
+console.log(School.pickSubstituteTeacher(['Jamal Crawford', 'Lou Williams', 'J. R. Smith', 'James Harden', 'Jason Terry', 'Manu Ginobli']))
+
+//--CREATING PRIMARY SUBCLASS
+
 class Primary extends School {
   constructor(name, level, numberOfStudents, pickupPolicy) {
     super(name, level, numberOfStudents);
@@ -30,7 +48,24 @@ class Primary extends School {
 
 const sebastien = new Primary('Primary Saint', 'primary', 500, 'ID');
 
-console.log(sebastien.name)
-console.log(sebastien.numberOfStudents)
-// sebastien.numberOfStudents(500);
-console.log(sebastien.numberOfStudents)
+//--TESTING PRIMARY SUBCLASS--
+// console.log(sebastien.name)
+// console.log(sebastien.numberOfStudents)
+// sebastien.numberOfStudents = 1000;
+// console.log(sebastien.numberOfStudents)
+console.log(sebastien.quickFacts())
+
+//--CREATING MIDDLE SUBCLASS
+class Middle extends School {
+  constructor(name, level, numberOfStudents) {
+    super(name, level, numberOfStudents)
+  }
+}
+
+//--CREATING HIGH SUBCLASS 
+class High extends School {
+  constructor(name, level, numberOfStudents, sportTeams) {
+    super(name, level, numberOfStudents);
+    this._sportTeams = sportTeams;
+  }
+}
